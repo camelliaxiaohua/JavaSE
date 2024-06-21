60d817080db3ffc64caf6a7394ae22b0c49600f1
Camellia.xiaohua <camellia.xiaohua@qq.com>
2024-06-21 10:14:46 +0800

    进程与线程的概述

---
+++ Part3/src/com/camellia/thread/Abegin_1/ThreadTest.java
@@ -0,0 +1,32 @@
+package com.camellia.thread.Abegin_1;
+
+/**
+ * 分析当前程序有多少个线程
+ * 当前程序除了GC线程之外只有一个主线程。
+ * 在JVM当中只有一个VM Stack。
+ * 这个栈底是main方法。
+ * 栈的顶部是m3方法。
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        System.out.println("main begin");
+        m1();
+        System.out.println("main end");
+    }
+
+    private static void m1() {
+        System.out.println("m1 begin");
+        m2();
+        System.out.println("m1 end");
+    }
+
+    private static void m2() {
+        System.out.println("m2 begin");
+        m3();
+        System.out.println("m2 end");
+    }
+
+    private static void m3() {
+        System.out.println("m3 execute");
+    }
+}
