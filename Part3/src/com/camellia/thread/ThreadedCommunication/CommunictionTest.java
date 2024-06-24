63124c8a12fc8a0ff4a3abfb0418277c23fdda3b
Camellia.xiaohua <2421195723@qq.com>
2024-06-24 12:18:33 +0800

    多线程通信（三个及三个以上个线程交替依次执行）

--- Part3/src/com/camellia/thread/ThreadedCommunication/CommunictionTest.java
+++ Part3/src/com/camellia/thread/ThreadedCommunication/CommunictionTest.java
@@ -1,6 +1,6 @@
 package com.camellia.thread.ThreadedCommunication;
 
-public class Test {
+public class CommunictionTest {
     public static void main(String[] args) {
         TestRunnable runnable = new TestRunnable();
         Thread thread1 = new Thread(runnable);
@@ -13,34 +13,45 @@ public class Test {
 }
 
 class TestRunnable implements Runnable {
-
-    private int count = 1;
+    private int count = 10;
+    private boolean t1Lock = true;
+    private boolean t2Lock = false;
+    private boolean t3Lock = false;
 
     @Override
     public void run() {
         while (true){
             synchronized (this){
+                // 释放被阻塞的线程，所以进来的即使不符合也不会占用，同时也唤醒wait的线程。
                 this.notify();
-                if(count == 10)break;
-                if(Thread.currentThread().getName().equals("Thread-0")){
-                    System.out.println(Thread.currentThread().getName()+"-->"+"A");
+                if (count <=0)break;
+                if(Thread.currentThread().getName().equals("Thread-0")&&t1Lock){
+                    t1Lock = false;
+                    t2Lock = true;
+                    System.out.println(Thread.currentThread().getName()+"\t:\t"+"A");
                     try {
                         this.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                if(Thread.currentThread().getName().equals("Thread-1")){
-                    System.out.println(Thread.currentThread().getName()+"-->"+"B");
+
+                if(Thread.currentThread().getName().equals("Thread-1")&&t2Lock){
+                    t2Lock = false;
+                    t3Lock = true;
+                    System.out.println(Thread.currentThread().getName()+"\t:\t"+"B");
                     try {
                         this.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                if(Thread.currentThread().getName().equals("Thread-2")){
-                    System.out.println(Thread.currentThread().getName()+"-->"+"C");
-                    count++;
+
+                if(Thread.currentThread().getName().equals("Thread-2")&&t3Lock){
+                    t3Lock = false;
+                    t1Lock = true;
+                    count--;
+                    System.out.println(Thread.currentThread().getName()+"\t:\t"+"C");
                     try {
                         this.wait();
                     } catch (InterruptedException e) {
@@ -48,7 +59,6 @@ class TestRunnable implements Runnable {
                     }
                 }
             }
-
         }
     }
 }
