892befd37f9b5e32afa18e5b171cf086277fc6bb
Camellia.xiaohua <2421195723@qq.com>
2024-06-26 19:31:38 +0800

    @Inherited注解

---
+++ Part3/src/com/camellia/annotation/meta/annotation/InheritedFather.java
@@ -0,0 +1,11 @@
+package com.camellia.annotation.meta.annotation;
+
+/**
+ * @Datetime: 2024/6/26下午5:21
+ * @author: Camellia.xioahua
+ */
+
+@RetentionAnnotation //@RetentionAnnotation注解中开启@Inherited
+public class InheritedFather {
+
+}
---
+++ Part3/src/com/camellia/annotation/meta/annotation/InheritedSub.java
@@ -0,0 +1,8 @@
+package com.camellia.annotation.meta.annotation;
+
+/**
+ * @Datetime: 2024/6/26下午5:21
+ * @author: Camellia.xioahua
+ */
+public class InheritedSub extends InheritedFather{
+}
